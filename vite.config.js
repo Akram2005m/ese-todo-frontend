7902293a4ec9032fdbccf88bafa86c2c1f9dfcf7
Akram2005m <akram.moubtassim@ada.ac.uk>
2026-03-27 15:09:41 +0000

    Remove conflicting Vite proxy config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://upgraded-space-pancake-jj754pjj56q924j4-8000.app.github.dev',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
 })
